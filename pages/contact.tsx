d14f7cf67822993059ae6f69883469b2ea3971d1
mundoenrique <mundoenrique@hotmail.com>
2022-07-25 16:51:56 -0400

    Nuevas paginas

---
+++ pages/contact.tsx
@@ -0,0 +1,9 @@
+const ContactPage = () => {
+  return (
+    <>
+      <h1>contact</h1>
+    </>
+  );
+};
+
+export default ContactPage;
---
+++ pages/theme-changer.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '../components/layouts';
+
+const ThemeChangerPage = () => {
+  return (
+    <Layout>
+      <h1>theme-changer</h1>
+    </Layout>
+  );
+};
+
+export default ThemeChangerPage;

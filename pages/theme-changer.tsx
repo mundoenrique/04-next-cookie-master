5f598a47ff1951218300e344fcb620f7c3f9283b
mundoenrique <mundoenrique@hotmail.com>
2022-07-25 17:07:25 -0400

    Diseño cambio de tema

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,9 +1,31 @@
+import { ChangeEvent, useState } from 'react';
+import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
+
 import { Layout } from '../components/layouts';
 
 const ThemeChangerPage = () => {
+  const [currentTheme, setCurrentTheme] = useState('light');
+  const onChangeTheme = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedTheme = event.target.value;
+
+    console.log(selectedTheme);
+    setCurrentTheme(selectedTheme);
+  };
+
   return (
     <Layout>
-      <h1>theme-changer</h1>
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>Tema</FormLabel>
+            <RadioGroup value={currentTheme} onChange={onChangeTheme}>
+              <FormControlLabel value="light" control={<Radio />} label="Light" />
+              <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+              <FormControlLabel value="custom" control={<Radio />} label="Custom" />
+            </RadioGroup>
+          </FormControl>
+        </CardContent>
+      </Card>
     </Layout>
   );
 };
